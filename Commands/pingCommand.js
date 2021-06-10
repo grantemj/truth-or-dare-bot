02c5060c926e1e30f592b70f647339027f8c7914
Devonte <69095599+devnote-dev@users.noreply.github.com>
2021-06-10 23:12:17 +0100

    Update pingCommand.js

--- Commands/pingCommand.js
+++ Commands/pingCommand.js
@@ -1,5 +1,5 @@
 export { pingCommand };
 import { client } from '../bot.js';
-async function pingCommand(message) {
+async function Command(message) {
     message.channel.send("Ping: " + client.ws.ping + "ms");
 }
