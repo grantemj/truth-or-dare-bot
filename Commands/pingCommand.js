eb3a254f8553b282b08dc98f091a2dbc116b68ec
Devonte <69095599+devnote-dev@users.noreply.github.com>
2021-06-11 03:01:13 +0100

    Update pingCommand.js

--- Commands/pingCommand.js
+++ Commands/pingCommand.js
@@ -1,8 +1,12 @@
-export { Command, SlashCommand };
+export { Command, SlashCommand, Meta };
 import { client } from '../bot.js';
 async function Command(message) {
     message.channel.send("Ping: " + client.ws.ping + "ms");
 }
 async function SlashCommand(interaction) {
     return interaction.editReply("Ping " + client.ws.ping + "ms");
+}
+const Meta = {
+    name: 'ping',
+    description: "ping Displays the average ping between the bot and Discord's webservers"
 }
